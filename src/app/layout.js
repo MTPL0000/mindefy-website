5375e6fe8fd09d9725f3e5a58ee2fdbbcbf508d8
MTPL0005-AbhishekDube <abhishek.dubey@mindefy.tech>
2025-07-15 15:45:55 +0530

    Add Google Analytics integration to layout.js
    - Included Google Analytics script for tracking and performance insights.
    - Configured the data layer and initialization for analytics tracking.

--- src/app/layout.js
+++ src/app/layout.js
@@ -130,6 +130,19 @@ export default function RootLayout({ children }) {
       <head>
         <link rel="canonical" href="https://mindefy.tech" />
         
+        {/* Google Analytics */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-EQVLJS1QWF"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-EQVLJS1QWF');
+            `,
+          }}
+        />
+        
         {/* PWA Meta Tags */}
         <meta name="application-name" content="Mindefy Technologies" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
