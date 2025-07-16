2a3516eb74dc52a29755d943c9f67e3510d945b6
MTPL0005-AbhishekDube <abhishek.dubey@mindefy.tech>
2025-07-16 15:20:47 +0530

    Add Facebook Meta Pixel integration to layout.js
    - Implemented Facebook Meta Pixel for enhanced tracking of page views.
    - Included both script and noscript tags to ensure functionality across different user scenarios.
    - This addition aims to improve analytics capabilities and user engagement.

--- src/app/layout.js
+++ src/app/layout.js
@@ -156,6 +156,34 @@ export default function RootLayout({ children }) {
           }}
         />
         
+        {/* Facebook Meta Pixel Code */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '2176304166214926');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        <noscript>
+          <img 
+            height="1" 
+            width="1" 
+            style={{display: 'none'}}
+            src="https://www.facebook.com/tr?id=2176304166214926&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
+        {/* End Facebook Meta Pixel Code */}
+        
         {/* PWA Meta Tags */}
         <meta name="application-name" content="Mindefy Technologies" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
