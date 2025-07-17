257b74a6dd15829d2ee2aad8eafdb5ddf44b5416
MTPL0005-AbhishekDube <abhishek.dubey@mindefy.tech>
2025-07-17 14:33:16 +0530

    Update ContactUs component styles for improved layout and spacing
    - Adjusted padding values for the contact section to enhance visual consistency across different screen sizes.
    - Modified margin values for the heading to improve overall aesthetics and user experience.

--- src/app/components/HomePage/ContactUs.js
+++ src/app/components/HomePage/ContactUs.js
@@ -228,9 +228,9 @@ export default function ContactUs() {
   };
 
   return (
-    <section id="contact" className="w-full py-12 sm:py-16 lg:py-20">
+    <section id="contact" className="w-full py-5 lg:py-10">
       <div className="container mx-auto px-4 sm:px-6 lg:px-8">
-        <h1 className="text-2xl sm:text-3xl lg:text-4xl xl:text-[2.12rem] font-semibold text-center mb-8 sm:mb-12 lg:mb-16">
+        <h1 className="text-2xl sm:text-3xl lg:text-4xl xl:text-[2.12rem] font-semibold text-center mb-8 lg:mb-12">
           Let's Get In Touch
         </h1>
 
