3df4958c6ad66d9ab0292e0fe416244bc14367d1
MTPL0005-AbhishekDube <abhishek.dubey@mindefy.tech>
2025-07-21 09:25:11 +0530

    Remove unused character images and clean up HeroSection component in HomePage

--- src/app/components/HomePage/HeroSection.js
+++ src/app/components/HomePage/HeroSection.js
@@ -5,25 +5,6 @@ import Link from "next/link";
 export default function HeroSection() {
   return (
     <section className="w-full min-h-screen bg-gradient-to-r from-[#f3f4ff] via-[#FFFFFF] to-[#fff3f0] flex items-center justify-center relative overflow-hidden px-4 sm:px-6">
-      {/* Floating 3D Characters - Hidden on mobile and small tablets */}
-      {/* <div className="absolute left-4 lg:left-8 xl:left-10 top-1/2 -translate-y-1/2 w-40 h-48 lg:w-[18rem] lg:h-[22rem] xl:w-[25rem] xl:h-[30rem] 2xl:w-[29.45rem] 2xl:h-[36.095rem] animate-pulse hidden md:block">
-        <Image
-          src="/images/boy.png"
-          alt="Character Left"
-          fill
-          className="object-contain"
-        />
-      </div> */}
-
-      {/* <div className="absolute right-4 lg:right-8 xl:right-10 top-1/2 -translate-y-1/2 w-40 h-48 lg:w-[18rem] lg:h-[22rem] xl:w-[25rem] xl:h-[30rem] 2xl:w-[29.45rem] 2xl:h-[36.095rem] animate-pulse hidden md:block">
-        <Image
-          src="/images/girl.png"
-          alt="Character Right"
-          fill
-          className="object-contain"
-        />
-      </div> */}
-
       <div className="mx-auto text-center z-10 max-w-4xl w-full">
         <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-8xl font-semibold text-center leading-tight">
           <span className="text-[#F6BC34]">Ideation</span>{" "}
