abcbe2dc7583098f208d69ebc2f830c711f94eb7
MTPL0012-LakhanPanwar <lakhan.panwar@mindefy.tech>
2025-11-25 12:52:14 +0530

    -Changed yourhour page name with the new keywords and updated this page path across all web pages. -Updated some content like metadata, schema, etc, for better SEO. -Updated blogs card design and added yourhour website blogs link on card click. -Removed old blog page files and image assets that were used on old blog pages. -Added new tab for blogs in footer section. -Added new tab for memolect in project dropdown in header section.

--- src/app/components/HomePage/Blog.js
+++ src/app/components/HomePage/Blog.js
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 export default function Blog() {
   return (
-    <section className="w-full py-12 sm:py-16 lg:py-20 px-4 sm:px-6 lg:px-8">
+    <section className="w-full py-12 sm:py-16 lg:py-18 px-4 sm:px-6 lg:px-8">
       <div className="container mx-auto">
         {/* Blog Header */}
         <div className="text-center mb-12 sm:mb-16 lg:mb-20">
@@ -20,66 +20,32 @@ export default function Blog() {
         {/* Blog Cards */}
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 sm:gap-8 lg:gap-10 mb-8 sm:mb-12 lg:mb-16">
           {/* Card 1 */}
-          <div className="bg-white rounded-3xl overflow-hidden hover:shadow-lg transition-shadow duration-300">
-            <div className="relative aspect-[1.28/1] w-full overflow-hidden">
+          <Link
+            href="https://www.yourhourapp.com/blog/yourhour-screentime-app-screen-time-tracker-mobile-addiction-show-behind-11"
+            target="_blank"
+            className="bg-white rounded-3xl overflow-hidden hover:shadow-lg hover:scale-105 transition-all cursor-pointer duration-300"
+          >
+            <div className="relative h-64 w-full overflow-hidden">
               <Image
-                src="/images/MS-1.webp"
-                alt="Microservices geometric pattern"
+                src="/images/YourHour-blog-1.webp"
+                alt="YourHour blog"
                 fill
                 className="rounded-2xl object-cover"
               />
             </div>
-            <div className="flex flex-col items-start gap-3 p-4 sm:p-6 lg:py-8 lg:px-2 text-[#30233D]">
+            <div className="flex flex-col items-start gap-3 p-4 sm:p-6 lg:pt-6 lg:px-2 text-[#30233D]">
               <h3 className="text-xl sm:text-2xl lg:text-2xl font-semibold">
-                Mastering Microservices: A Modern Approach
+                "MOBILE ADDICTION" - The Show Behind!
               </h3>
               <p className="text-sm sm:text-base lg:text-lg text-justify font-normal leading-relaxed">
-                Micro-services in this era is getting so much attention from
-                articles, social media, conference presentation
-              </p>
-              <div className="text-[#6A53E1] flex items-center text-base sm:text-lg font-medium cursor-pointer hover:underline transition-colors">
-                <Link href="/blog/microservices" className="flex items-center">
-                  Read More
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-4 w-4 sm:h-5 sm:w-5 ml-2"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth={2}
-                      d="M14 5l7 7m0 0l-7 7m7-7H3"
-                    />
-                  </svg>
-                </Link>
-              </div>
-            </div>
-          </div>
-
-          {/* Card 2 */}
-          <div className="bg-white rounded-3xl overflow-hidden hover:shadow-lg transition-shadow duration-300">
-            <div className="relative aspect-[1.28/1] w-full overflow-hidden">
-              <Image
-                src="/images/TDB-1.webp"
-                alt="Hand drawing upward curve"
-                fill
-                className="rounded-2xl object-cover"
-              />
-            </div>
-            <div className="flex flex-col items-start gap-3 p-4 sm:p-6 lg:py-8 lg:px-2 text-[#30233D]">
-              <h3 className="text-lg sm:text-xl lg:text-2xl font-semibold leading-tight lg:mb-1">
-                The Difference Between "Average And Success"
-              </h3>
-              <p className="text-sm sm:text-base lg:text-lg text-justify font-normal leading-relaxed">
-                The amount of free time, we as humans have now was never been
-                the same as before.
+                It is often said, “Change is the inevitable truth”. Things
+                change, situations change and people change. It spares no
+                one....
               </p>
               <div className="text-[#6A53E1] flex items-center text-base sm:text-lg font-medium cursor-pointer hover:underline transition-colors">
                 <Link
-                  href="/blog/average-and-success"
+                  href="https://www.yourhourapp.com/blog/yourhour-screentime-app-screen-time-tracker-mobile-addiction-show-behind-11"
+                  target="_blank"
                   className="flex items-center"
                 >
                   Read More
@@ -100,29 +66,82 @@ export default function Blog() {
                 </Link>
               </div>
             </div>
-          </div>
+          </Link>
+
+          {/* Card 2 */}
+          <Link
+            href="https://www.yourhourapp.com/blog/screen-time-parental-control-screen-time-helper-young-vs-old-smart-phones-seperating-generations-10"
+            target="_blank"
+            className="bg-white rounded-3xl overflow-hidden hover:shadow-lg hover:scale-105 transition-all cursor-pointer duration-300"
+          >
+            <div className="relative h-64 w-full overflow-hidden">
+              <Image
+                src="/images/YourHour-blog-2.webp"
+                alt="YourHour blog"
+                fill
+                className="rounded-2xl object-cover"
+              />
+            </div>
+            <div className="flex flex-col items-start gap-3 p-4 sm:p-6 lg:pt-6 lg:px-2 text-[#30233D]">
+              <h3 className="text-lg sm:text-xl lg:text-2xl font-semibold leading-tight lg:mb-1">
+                "YOUNG V/S THE OLD!" Smart Phones Seperating Generations!
+              </h3>
+              <p className="text-sm sm:text-base lg:text-lg text-justify font-normal leading-relaxed">
+                Addiction is injurious to health! And while most narcotics
+                damage a person’s Biological wellbeing, Smart Phones attack....
+              </p>
+              <div className="text-[#6A53E1] flex items-center text-base sm:text-lg font-medium cursor-pointer hover:underline transition-colors">
+                <Link
+                  href="https://www.yourhourapp.com/blog/screen-time-parental-control-screen-time-helper-young-vs-old-smart-phones-seperating-generations-10"
+                  target="_blank"
+                  className="flex items-center"
+                >
+                  Read More
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-4 w-4 sm:h-5 sm:w-5 ml-2"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M14 5l7 7m0 0l-7 7m7-7H3"
+                    />
+                  </svg>
+                </Link>
+              </div>
+            </div>
+          </Link>
 
           {/* Card 3 */}
-          <div className="bg-white rounded-3xl overflow-hidden hover:shadow-lg transition-shadow duration-300 md:col-span-2 lg:col-span-1">
-            <div className="relative aspect-[1.28/1] w-full overflow-hidden">
+          <Link
+            href="https://www.yourhourapp.com/blog/phone-addiction-controller-screen-time-app-usage-tracker-mindfulness-productivity-4"
+            target="_blank"
+            className="bg-white rounded-3xl overflow-hidden hover:shadow-lg hover:scale-105 transition-all cursor-pointer duration-300 md:col-span-2 lg:col-span-1"
+          >
+            <div className="relative h-64 w-full overflow-hidden">
               <Image
-                src="/images/TOA-1.webp"
+                src="/images/YourHour-blog-3.webp"
                 alt="Home office setup"
                 fill
                 className="rounded-2xl object-cover"
               />
             </div>
-            <div className="flex flex-col items-start gap-3 p-4 sm:p-6 lg:py-8 lg:px-2 text-[#30233D]">
+            <div className="flex flex-col items-start gap-3 p-4 sm:p-6 lg:pt-6 lg:px-2 text-[#30233D]">
               <h3 className="text-xl sm:text-2xl lg:text-2xl lg:mb-8 font-semibold">
-                The Office Away From Office!
+                Mindfulness and Productivity!
               </h3>
               <p className="text-sm sm:text-base lg:text-lg text-justify font-normal leading-relaxed">
-                Companies, in the face of this global pandemic, have asked their
-                employees to Work From Home.
+                The esteemed poet, Robert Frost, once wrote, “two roads divulged
+                in a yellow wood…” he was talking, centuries ago....
               </p>
               <div className="text-[#6A53E1] flex items-center text-base sm:text-lg font-medium cursor-pointer hover:underline transition-colors">
                 <Link
-                  href="/blog/the-office-away"
+                  href="https://www.yourhourapp.com/blog/phone-addiction-controller-screen-time-app-usage-tracker-mindfulness-productivity-4"
+                  target="_blank"
                   className="flex items-center"
                 >
                   Read More
@@ -143,7 +162,7 @@ export default function Blog() {
                 </Link>
               </div>
             </div>
-          </div>
+          </Link>
         </div>
 
         {/* View All Button */}
