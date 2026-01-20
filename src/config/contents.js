8221e2d48f141918ae57a361ada05087e9ba3a32
MTPL0005-AbhishekDube <abhishek.dubey@mindefy.tech>
2026-01-20 17:58:35 +0530

    Updated aspect ratio for yourHour content from 886/1140 to 665/855 for improved image rendering accuracy

--- src/config/contents.js
+++ src/config/contents.js
@@ -34,7 +34,7 @@ export const yourHourContent = {
   ],
   caseStudyLink: "/yourhour-screentime-app",
   caseStudyText: "VIEW CASE STUDY",
-  aspectRatio: "886/1140",
+  aspectRatio: "665/855",
 };
 
 export const JEGOContent = {
