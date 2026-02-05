87bc93d5c5739ecec26a1ec99ecf212b146d6eb9
MTPL0012-LakhanPanwar <lakhan.panwar@mindefy.tech>
2026-02-05 16:59:50 +0530

    -Enabled Server-Side Rendering for the content sections.

--- src/components/IOS/IOSContent.js
+++ src/components/IOS/IOSContent.js
@@ -3,9 +3,7 @@
 import dynamic from "next/dynamic";
 import IOSHero from "./IOSHero";
 
-const ProcessSection = dynamic(() => import("./ProcessSection"), {
-  ssr: false,
-});
+const ProcessSection = dynamic(() => import("./ProcessSection"));
 
 export default function IOSContent() {
   return (
